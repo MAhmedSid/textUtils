c919691bc26da2d345c098ace5f4bb93ce384605
MAhmedSid <mahmedsiddiqui333@gmail.com>
2022-12-25 13:09:26 +0500

    first commit with 0 errors found.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useState } from 'react';
+import './App.css';
+import About from './Components/About';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+document.body.style.height = "100vh"
 function App() {
+
+
+
+  const [mode, setMode] = useState("light")
+  const [alert, setAlert] = useState(null)
+
+
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type,
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      document.body.style.background = "linear-gradient(180deg, rgb(27, 27, 42) 0%, rgb(16, 21, 44) 72%, rgb(6, 5, 42) 100%)"
+      document.body.style.color = "white"
+      setMode("dark")
+
+    } else {
+      document.body.style.background = "white";
+      document.body.style.color = "black"
+      setMode("light")
+
+    }
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar title={"TextUtils"} toggle={toggleMode} mode={mode} />
+        <Alert alert={alert} />
+
+        <Routes>
+          <Route index exact path="/home" element={<TextForm heading={"Enter your text to analyze"} alert={showAlert} firstbutton={"Convert to Upper Case"} secondbutton={'Convert to Lower Case'} mode={mode} />} />
+          <Route exact path="/about" element={<About mode={mode} />} />
+        </Routes>
+      </Router>
+
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+
+
+
+
+
+export default function Navbar(props) {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" style={{ fontSize: "2rem" }} to="/home">{props.title}</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link " aria-current="page" to="/home">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+            </ul>
+            <div>
+              <div className="form-check form-switch ">
+                <input className="form-check-input bg-danger text-dark border-dark shadow-none" onClick={props.toggle} type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                <label className="form-check-label text-white" htmlFor="flexSwitchCheckDefault">{props.mode === "light" ? "Enable Dark Mode" : "Disable Dark Mode"}</label>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+}
